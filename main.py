09a673584d48f2a3a84ab18f83731d9f154aa002
Hanashiko <129686115+Hanashiko@users.noreply.github.com>
2024-09-02 19:14:13 +0000

    Add files via upload

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from typing import List
 
 def countOdds(low: int, high: int) -> int:
-    count: int = 0
-    for i in range(low, high + 1):
-        if i % 2 != 0:
-            count += 1
-    return count
+    if low % 2 == 0:
+        low += 1
+    if high % 2 == 0:
+        high -= 1
+    return (high - low) // 2 + 1
 
 def main() -> None:
     print(countOdds(3, 7))
